14703b7f00c2fdca3d31f1fd2949c9ee22f8b814
Fabio Moraes <fabio.santosmoraes@hotmail.com>
2021-07-21 10:10:33 -0300

    fix: novo calculo para "national holidays"

--- src/easter/index.ts
+++ src/easter/index.ts
@@ -1,24 +1,21 @@
+import { add } from 'date-fns';
+
 export function _calculateEaster(year: number): Date {
-  const C = Math.floor(year / 100);
-  const N = year - 19 * Math.floor(year / 19);
-  const K = Math.floor((C - 17) / 25);
-  let I = C - Math.floor(C / 4) - Math.floor((C - K) / 3) + 19 * N + 15;
-  I = I - 30 * Math.floor(I / 30);
-  I =
-    I -
-    Math.floor(I / 28) *
-      (1 -
-        Math.floor(I / 28) *
-          Math.floor(29 / (I + 1)) *
-          Math.floor((21 - N) / 11));
-  let J = year + Math.floor(year / 4) + I + 2 - C + Math.floor(C / 4);
-  J = J - 7 * Math.floor(J / 7);
-  const L = I - J;
-  const M = 3 + Math.floor((L + 40) / 44);
-  const D = L + 28 - 31 * Math.floor(M / 4);
+  let M = 3,
+    G = (year % 19) + 1,
+    C = ~~(year / 100) + 1,
+    L = ~~((3 * C) / 4) - 12,
+    E = (11 * G + 20 + ~~((8 * C + 5) / 25) - 5 - L) % 30,
+    D;
+  E < 0 && (E += 30);
+  ((E == 25 && G > 11) || E == 24) && E++;
+  (D = 44 - E) < 21 && (D += 30);
+  (D += 7 - ((~~((5 * year) / 4) - L - 10 + D) % 7)) > 31 &&
+    ((D -= 31), (M = 4));
+
   const month = M < 10 ? '0' + M : M;
   const day = D < 10 ? '0' + D : D;
   const easterDate = year + '-' + month + '-' + day;
 
-  return new Date(easterDate);
+  return add(new Date(easterDate), { minutes: new Date().getTimezoneOffset() });
 }
--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 const { currentDate, isBusinessDay, getBusinessDay, getNextBusinessDay, getNationalHolidays, isHoliday, getDate, getYear, formatDate, TypeDate } = require("../dist");
 
-console.log('isBussinesDay -', isBusinessDay());
+console.log('isBussinesDay -', isBusinessDay('2021-01-01'));
 console.log('isBussinesDay -', isBusinessDay('2021-07-17'));
 console.log('getBusinessDay -', getBusinessDay('2021-12-25'));
-console.log('getNextBusinessDay -', getNextBusinessDay(new Date(), 5));
+console.log('getNextBusinessDay -', getNextBusinessDay('2021-01-01'));
 console.log('getNationalHolidays -', getNationalHolidays());
 console.log('isHoliday -', isHoliday());
 console.log('isHoliday (ano novo)-', isHoliday('2021-01-01'));
@@ -13,3 +13,7 @@ console.log('currentDate -', currentDate());
 console.log('getYear -', getYear());
 console.log('formatDate -', formatDate('2021-07-19', TypeDate.BR));
 console.log('formatDate -', formatDate('2021-07-20T23:59:00.371Z', TypeDate.BR));
+
+console.log('getNationalHolidays -', getNationalHolidays('2024'));
+
+
